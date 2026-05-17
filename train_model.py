a784fbef3079517a1d837b3af62b30ad5dd5c377
yash_vasoya_07 <yashvasoya897@gmail.com>
2026-05-17 18:16:27 +0530

    Add files via upload

---
+++ train_model.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import joblib
+
+# sample dataset (example)
+data = pd.DataFrame({
+    "hours": [1,2,3,4,5],
+    "attendance": [60,70,80,90,95],
+    "sleep": [5,6,7,8,9],
+    "prev_marks": [40,50,60,70,80],
+    "marks": [45,55,65,75,85]
+})
+
+X = data[["hours","attendance","sleep","prev_marks"]]
+y = data["marks"]
+
+model = LinearRegression()
+model.fit(X, y)
+
+joblib.dump(model, "model.pkl")
+
+print("Model trained successfully!")
